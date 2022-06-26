f9cbd2b7539ffc22ddf15b9e8ad5c2e281eecabf
ohtaekwon <ohtk1119@gmail.com>
2022-06-26 15:21:01 +0900

    API-Data-Promblem-Slove

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 export async function getFeeds() {
-  const ref = window.location.href;
+  const ref = window.location.origin;
   const response = await fetch(`${ref}/data/mock.json`);
   const body = await response.json();
   return body;
 }
-
+// ${ref}/data/mock.json
 // export async function getFeeds() {
 //   const response = await fetch('http://localhost:3000/data/mock.json');
 //   const body = await response.json();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyles from './styles/globalStyles';
+import LoginPage from './pages/LoginPage';
+import MainPage from './pages/MainPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
